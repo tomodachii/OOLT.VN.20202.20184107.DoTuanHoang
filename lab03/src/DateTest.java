dcc8b23d223ba5a711264ff7ea2f4af5adf73afc
Do Tuan Hoang <tuanhoang2do2311@gmail.com>
2021-03-17 17:12:50 +0700

    Modified DateTest.java

--- lab03/src/DateTest.java
+++ lab03/src/DateTest.java
@@ -3,8 +3,15 @@ public class DateTest {
 
 	public static void main(String[] args) {
 		MyDate date = new MyDate();
+		date.print();
 		date.accept();
 		date.print();
+		
+		MyDate date1 = new MyDate(16,3,2020);
+		date1.print();
+		
+		MyDate date2 = new MyDate("17/3/2020");
+		date2.print();
 	}
 
 }
